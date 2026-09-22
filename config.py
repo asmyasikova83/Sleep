e25a7772ddd3592760826a0f5beb2f78d1f89a0a
asmyasnikova83 <asmyasnikova83@gmail.com>
2026-09-22 11:41:55 +0300

    minor

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ target_eeg = ['C3', 'C4', 'EEG C4-M1', 'EEG C4', 'C4-Ref', 'C4 - A1 - A2', 'C4-P
 target_eog = ['EOG E2-M2', 'EOG E1-M1', 'ROC-LOC']
 target_emg = ['EMG chin', 'CHIN', 'EMG1-EMG2']
 
+# Settings from https://yasa-sleep.org/tutorials/quickstart.html
 #Resample
 resample_rate = 100
 
